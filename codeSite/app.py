0dbb6caa6965a10a50ffd3993f8be47da21e79bb
J18a <102650208+J18a@users.noreply.github.com>
2022-10-23 00:55:26 -0700

    Add files via upload

--- codeSite/app.py
+++ codeSite/app.py
@@ -9,7 +9,7 @@ with col1:
     st.title(body='Honeytree Appliance Repair')
     st.header('Error Code Search')
 
-    st.write('For now this is only good for **Whirlpool Washers**.')
+    st.write('For now this is only good for **Washers**.')
     st.write('Its still a work in progress but you get the basics.')
 with col2:
     st.image("C:\\Users\\Honey\\Downloads\\honeytree logos\\Png\\Honeytreesolutions Logo_Dark BCGD-Mark.png")
